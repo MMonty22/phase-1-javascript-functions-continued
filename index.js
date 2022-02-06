e5bf8b6cf180fac2753b0836004b9f6aaa532607
Michael <michael.c.montgomery@gmail.com>
2022-02-06 14:43:10 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity) {
+    if(activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else return "This Saturday, I want to roller-skate!"
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(task) {
+    if(task) {
+        return `This Monday, I will ${task}.`
+    } else return 'This Monday, I will go to the office.'
+}
+
+mondayWork('work from home');
+
+function wrapAdjective (adj = "*") {
+    return function (adj2 = "special") {
+       console.log("adj", adj)
+       console.log("adj2", adj2)
+        return `You are ${adj}${adj2}${adj}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
